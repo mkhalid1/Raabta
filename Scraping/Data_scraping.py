0caaa8b873f7e4c40937b8273062494935268dd6
Muhammad Junaid <mkhalid.bese16seecs@seecs.edu.pk>
2020-04-10 21:40:49 +0500

    Updated remaining files

--- Scraping/Data_scraping.py
+++ Scraping/Data_scraping.py
@@ -20,10 +20,10 @@ reddit = praw.Reddit(client_id='0rr-7cBdvQvn_A',
 
 # get posts from the subreddits related to donations 
 hot_post_1 = reddit.subreddit('donate').hot(limit= 5000) 
-hot_post_2 = reddit.subreddit('Assistance').hot(limit= 5000) 
+hot_post_2 = reddit.subreddit('Assistance').hot(limit= 5000) # Offers
 hot_post_3 = reddit.subreddit('Charity').hot(limit= 5000) 
 hot_post_4 = reddit.subreddit('Donation').hot(limit= 5000) 
-hot_post_5 = reddit.subreddit('gofundme').hot(limit= 5000) 
+hot_post_5 = reddit.subreddit('gofundme').hot(limit= 5000) # lots of categories
 hot_post_6 = reddit.subreddit('RandomKindness').hot(limit = 5000) 
 hot_post_7 = reddit.subreddit('donationrequest').hot(limit = 5000)
 
@@ -52,35 +52,13 @@ for post in hot_post_6:
 posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
 #posts
 
-
 df = pd.DataFrame(data=posts)
-
-
-
 dataframe = df.to_csv(r'donations.csv')
-
-
 # ## Data Processing
 
-
-
-
 df = pd.read_csv('donations.csv')
-
-
-
-
 df = df.drop(['id', 'subreddit', 'num_comments', 'url', 'created'],1)
-
-
-
 df = df[['title', 'score','body']]
-
-
-
-
 print(df.head())
-
-
 df.shape
 dataframe = df.to_csv(r'donations.csv')
